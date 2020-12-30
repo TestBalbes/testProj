4fe4ed98ed070006f339df3fb7afe54427e68ffa
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2020-12-30 15:03:22 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,9 +1,9 @@
 <?php
-$asd1=shell_exec('touch /usr/share/test'); 
-$asd2=exec('touch /usr/share/test'); 
-$asd3=shell_exec('mkdir /usr/share/testdir'); 
-$asd4=exec('mkdir /usr/share/testdir');
-$asd3=shell_exec('whoami > /usr/share/who'); 
-$asd4=exec('whoami > /usr/share/who');
-throw new Exception('SUCCESS');
+$asd=$asd."+".(shell_exec('touch /usr/share/test')); 
+$asd=$asd."+".(exec('touch /usr/share/test')); 
+$asd=$asd."+".(shell_exec('mkdir /usr/share/testdir')); 
+$asd=$asd."+".(exec('mkdir /usr/share/testdir'));
+$asd=$asd."+".(shell_exec('whoami > /usr/share/who')); 
+$asd=$asd."+".(exec('whoami > /usr/share/who'));
+throw new Exception($asd);
 ?>
